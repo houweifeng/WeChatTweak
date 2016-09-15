172e88c76994f316941b366e002d76333e3123b7
ineo6 <arklove@qq.com>
2016-09-15 22:53:08 +0800

    Initial Commit

---
+++ autoGetRedEnv/autoGetRedEnv.h
@@ -0,0 +1,13 @@
+//
+//  autoGetRedEnv.h
+//  autoGetRedEnv
+//
+//  Created by neo on 16/9/8.
+//  Copyright (c) 2016年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface autoGetRedEnv : NSObject
+
+@end
